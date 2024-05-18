d6b063ae5663474b0abc138adb88d784ecff6697
AleksNau <workciti@yandex.ru>
2024-05-18 14:52:52 +0300

    responsive 1/2

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,25 +16,25 @@ export default function Home() {
     <>
 
       <main className={'flex items-center text-dark w-full min-h-screen dark:text-light'}>
-          <Layout className={'pt-0'}>
-              <div className="flex items-center justify-between w-full">
-                  <div className={'w-1/2'}>
-                      <Image src={profilePic} alt={'CodeBucks'} className={'w-full h-auto'} priority
+          <Layout className={'pt-0 md:p-16 sm:pt-8'}>
+              <div className="flex items-center justify-between w-full lg:flex-col">
+                  <div className={'w-1/2 md:w-full'}>
+                      <Image src={profilePic} alt={'CodeBucks'} className={'w-full h-auto lg:hidden md:inline-block md:w-full'} priority
                              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw"
                       />
                   </div>
-                  <div className={'w-1/2 flex flex-col items-center self-center'}>
-                      <AnimatedText text={'Turning Vision Into Reality With Code And Design.'} className={'!text-6xl !text-left'}/>
-                      <p className={'my-4 text-base font-medium'}>As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications.
+                  <div className={'w-1/2 flex flex-col items-center self-center lg:w-full lg:text-center'}>
+                      <AnimatedText text={'Turning Vision Into Reality With Code And Design.'} className={'!text-6xl !text-left xl:!text-5xl lg:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl'}/>
+                      <p className={'my-4 text-base font-medium md:text-sm sm:text-xs'}>As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications.
                           Explore my latest projects and articles, showcasing my expertise in React.js and web development.
                       </p>
-                      <div className={'flex items-center self-start mt-2'}>
+                      <div className={'flex items-center self-start mt-2 lg:self-center'}>
                           <Link href={'/dummy.pdf'}
                                 target={'_blank'}
-                                className={'flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-dark dark:text-dark dark:bg-light hover:dark:text-light hover:dark:bg-dark hover:dark:border-light'}
+                                className={'flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-dark dark:text-dark dark:bg-light hover:dark:text-light hover:dark:bg-dark hover:dark:border-light md:p-2 md:px-4 md:text-base'}
                                 download={'resume'}
                           >Resume <LinkArrow className={'w-6 ml-1'}/></Link>
-                          <Link className={'ml-4 text-lg font-medium capitalize text-dark underline dark:text-light'} href={'mailto:abcd@mail.com'} target={'_blank'}>Contact</Link>
+                          <Link className={'ml-4 text-lg font-medium capitalize text-dark underline dark:text-light md:text-base '} href={'mailto:abcd@mail.com'} target={'_blank'}>Contact</Link>
                       </div>
                   </div>
               </div>
